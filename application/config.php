5addb45c54b0b56392d7f60d1466001dc8997b5b
wujb <1120950304@qq.com>
2018-01-01 12:57:38 +0800

    [MOD] 修改配置文件中替换字符串public的定义

--- application/config.php
+++ application/config.php
@@ -142,7 +142,7 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__PUBLIC__' => '/2014guanga/thinkphp_5.0.10_full/public/'
+        '__PUBLIC__' => '/community/public/'
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
