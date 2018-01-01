7b657c99e21d4d8ddf5c2a504274ba2fd6d574ea
wujb <1120950304@qq.com>
2018-01-01 17:22:19 +0800

    [MOD] 更改数据库配置

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '118.89.63.192',
     // 数据库名
-    'database'        => 'guangda_kcsj',
+    'database'        => 'ttt',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'root2',
     // 密码
-    'password'        => '',
+    'password'        => '123456',
     // 端口
     'hostport'        => '',
     // 连接dsn
--- application/index/controller/News.php
+++ application/index/controller/News.php
@@ -6,7 +6,7 @@ class News extends \think\Controller
     public function preview()
     {
         // 查询这篇文章的信息
-        $info = db('news')->where("id=".input('id'))->find();
+        $info = db('news')->where("id=".input('id'))->select();
 
         $this->assign('info',$info);
         
